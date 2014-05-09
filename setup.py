295a7f70bc6a227c9a1b354a6fe208f879681083
Antonio Cuni <anto.cuni@gmail.com>
2014-05-09 17:09:01 +0200

    add a simple setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name = 'cffi-shm',
+	version = '0.1',
+	packages = ['shm'],
+	author = "Antonio Cuni",
+	author_email = "anto.cuni@gmail.com",
+	description = "cffi shm",
+	zip_safe=False,
+)
