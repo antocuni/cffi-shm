f05dba174a201556256d3422e2c6282ebbdd0a99
Antonio Cuni <anto.cuni@gmail.com>
2014-09-05 17:34:54 +0200

    a passing test

--- shm/testing/test_deque.py
+++ shm/testing/test_deque.py
@@ -85,3 +85,13 @@ def test___iter__(pyffi):
     d.append(5)
     d.append(6)
     assert list(d) == [2, 3, 4, 5, 6]
+
+def test___setitem__(pyffi):
+    DT = pyffi.deque('long')
+    #
+    # start with a buffer of 4 items and offset==1
+    d = DT([1, 2, 3])
+    assert d.popleft() == 1
+    d[0] = 20
+    d[1] = 30
+    assert list(d) == [20, 30]
